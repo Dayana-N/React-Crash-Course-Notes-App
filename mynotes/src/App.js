4e96a4f14d4517a85d83b08710514eb5cfde862c
Dayana <dayana.nashkova@yahoo.com>
2024-03-23 20:39:18 +0000

    feat: display notes by id

--- mynotes/src/App.js
+++ mynotes/src/App.js
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 
 import Header from './components/Header';
 import NotesPage from './pages/NotesPage';
+import NotePage from './pages/NotePage';
 import './App.css';
 
 function App() {
@@ -10,6 +11,7 @@ function App() {
       <div className='App'>
         <Header />
         <Route path='/' component={NotesPage} exact />
+        <Route path='/note/:id' component={NotePage} />
       </div>
     </Router>
   );
