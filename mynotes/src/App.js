fda95ead428bce4f3e4148c8710026c01b9e43c9
Dayana <dayana.nashkova@yahoo.com>
2024-03-23 19:45:02 +0000

    feat: install react router dom

--- mynotes/src/App.js
+++ mynotes/src/App.js
@@ -1,13 +1,17 @@
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+
 import Header from './components/Header';
 import NotesPage from './pages/NotesPage';
 import './App.css';
 
 function App() {
   return (
-    <div className='App'>
-      <Header />
-      <NotesPage />
-    </div>
+    <Router>
+      <div className='App'>
+        <Header />
+        <Route path='/' component={NotesPage} exact />
+      </div>
+    </Router>
   );
 }
 
